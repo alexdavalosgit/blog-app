53ce8c2b105d3826b9a9cf5899fbbd2f54e03bf4
Alex <iambethharmon@protonmail.com>
2023-02-16 15:22:35 -0500

    added register functionality

--- backend/app.js
+++ backend/app.js
@@ -16,10 +16,13 @@ mongoose.set("strictQuery", false);
 // CORS error
 app.use((req, res, next) => {
   //allow access from every, elminate CORS
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.removeHeader("x-powered-by");
   //set the allowed HTTP methods to be requested
-  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
   //headers clients can use in their requests
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   //allow request to continue and be handled by routes
--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,19 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, Container } from "react-bootstrap";
+import { baseAPI } from "../../utils";
 
 function Register() {
+  // State
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false);
+    try {
+      const url = `${baseAPI}/auth/register`;
+      console.log("url", url);
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password,
+        }),
+      });
+      if (res.ok) {
+        window.location.replace("/login");
+        console.log("res", res.data);
+      } else {
+        throw new Error(`Failed to fetch data with status ${res.status}`);
+      }
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <h2>Register</h2>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="username"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -21,12 +64,19 @@ function Register() {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
           Register
         </Button>
+        {error && (
+          <span className="text-danger px-2">Something went wrong!</span>
+        )}
       </Form>
     </Container>
   );
