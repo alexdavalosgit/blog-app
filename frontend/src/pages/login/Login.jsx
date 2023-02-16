79a99e6f5d806bdf221533197acfce2591f94420
Alex <iambethharmon@protonmail.com>
2023-02-16 16:57:10 -0500

    added auth context, login page calls api

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,19 +1,57 @@
-import React from "react";
+import React, { useRef, useContext } from "react";
 import { Button, Form, Container } from "react-bootstrap";
+import { AuthContext } from "../../context/authcontext/AuthContext";
+import { baseAPI } from "../../utils";
 
 function Login() {
+  // const
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFetching } = useContext(AuthContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const url = `${baseAPI}/auth/login`;
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: userRef.current.value,
+          password: passwordRef.current.value,
+        }),
+      });
+      console.log("res", res);
+      if (res.ok) {
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      } else {
+        throw new Error(`Login failure with status ${res.status}`);
+      }
+    } catch (error) {
+      console.log(error);
+      dispatch({ type: "LOGIN_FAILURE" });
+    }
+  };
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <h2>Login</h2>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control ref={userRef} type="username" placeholder="Username" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
